d45154a7300487d85c26ed05746b86225a618071
PhatStraw <kevindsimsjr@gmail.com>
2023-11-25 09:02:28 -0800

    v1

---
+++ README.md
@@ -0,0 +1,52 @@
+# OpenAI GPT-4 Code Refactor
+
+This Visual Studio Code extension leverages OpenAI's GPT-4 model to refactor your code. It provides an easy and quick way to improve and optimize your code with the power of AI.
+
+## Features
+
+- Refactor code using OpenAI's GPT-4 model.
+- Easy to use within VSCode.
+- Supports a wide range of programming languages (as supported by GPT-4).
+
+## Installation
+
+1. Install the Extension: Search for "airefactor" in the VSCode Extensions Marketplace and install it.
+2. Set API Key: 
+
+    - After installing the extension, open the Command Palette in VS Code by pressing `Ctrl+Shift+P` (or `Cmd+Shift+P` on macOS).
+    - Type [Preferences: Open User Settings](file:///Users/kevinsims/Code/airefactor/README.md#27%2C9-27%2C9) and select it. This will open the settings.json file.
+    - In the search bar at the top, type `airefactor.openaiApiKey` to find the setting contributed by your extension.
+    - Enter your OpenAI API key in the input box for the `airefactor.openaiApiKey` setting.
+    - Save the settings.json file and restart VS Code.
+
+## Usage
+
+1. Open the file you want to refactor in VSCode.
+2. Press `Ctrl+Shift+P` (or `Cmd+Shift+P` on Mac) to open the Command Palette.
+3. Type "Refactor with GPT-4" and select the command.
+4. The extension will send your code to OpenAI's GPT-4 model and replace your current code with the refactored version.
+
+## Configuration
+
+- **API Key**: The extension requires an OpenAI API key to function. This key must be set in your system's environment variables as `OPENAI_API_KEY`.
+- **Model Configuration**: The extension uses the [text-davinci-004](file:///Users/kevinsims/Code/airefactor/README.md#48%2C52-48%2C52) model by default. You can modify the model used by editing the `extension.js` file if necessary.
+
+## Troubleshooting
+
+If you encounter any issues:
+
+- Ensure your OpenAI API key is correctly set in your environment variables.
+- Check if the OpenAI API service is operational.
+- Restart VSCode if the extension does not appear or work correctly.
+
+## Support & Contributions
+
+For support, please open an issue in the GitHub repository. Contributions are welcome! Feel free to fork the repository and submit pull requests.
+
+## License
+
+[Specify the License Here]
+
+## Acknowledgments
+
+This extension was built using OpenAI's GPT-4 API.
---
+++ extension.js
@@ -0,0 +1,126 @@
+const vscode = require("vscode");
+const OpenAI = require("openai");
+
+let config = vscode.workspace.getConfiguration("refactorme");
+const openai = new OpenAI({
+  apiKey: config.get("openaiApiKey"),
+});
+
+function getActiveFileContent() {
+    const editor = vscode.window.activeTextEditor;
+    if (!editor) {
+      return null;
+    }
+  
+    const selection = editor.selection;
+    if (!selection.isEmpty) {
+      // If there is a selection, return the selected text
+      return editor.document.getText(selection);
+    } else {
+      // If there is no selection, return the entire document's text
+      return editor.document.getText();
+    }
+  }
+
+async function refactorWithIoenAI(code) {
+  const response = await openai.chat.completions.create({
+    model: "gpt-4-1106-preview",
+    max_tokens: 4096,
+    temperature: 0,
+    response_format: { type: "json_object" },
+    messages: [
+      {
+        role: "system",
+        content: `
+            You are asenior code developer assistant bot that is fed files of code from an ide. You are to refactor this code but keep all functionality.
+
+            Key Instructions:
+            1. Preserve Functionality: The refactored code must maintain all original functionalities, scripts, and styling exactly as in the original snippet. Any change in functionality is unacceptable.
+            2. Avoid Pseudocode: Deliver the refactored code in its complete and executable form. Do not include pseudocode, placeholders, theoretical suggestions or comments, write full code.
+            3. No Abstractions or New Files: Refactor the code within the given snippet itself. Do not abstract parts of the code into separate functions or files that were not in the original snippet.
+            4. Adherence to Best Practices: Ensure your refactoring aligns with best practices suitable for a top-tier tech environment. This includes clean code principles, proper commenting, and efficient use of resources.
+            5. Test for Production: Confirm that the refactored code is production-ready, meaning it's tested, reliable, and won't introduce new issues or dependencies.
+            6. Check syntax and make sure the code is usable.
+            7. Do not remove any code, without replacing it. no brevity.
+            
+            Please structure your response in the following JSON format:
+            {
+              "refactoredCode": "<Insert your fully refactored and functional code here>"
+            }
+            `,
+      },
+      {
+        role: "user",
+        content: `${code}`,
+      },
+    ],
+  });
+
+  const parsed = JSON.parse(response.choices[0].message.content);
+
+  return parsed.refactoredCode;
+}
+
+function updateFileWithRefactoredCode(refactoredCode) {
+    const editor = vscode.window.activeTextEditor;
+    if (!editor) {
+      vscode.window.showErrorMessage("No active text editor available.");
+      return;
+    }
+  
+    const document = editor.document;
+    const selection = editor.selection;
+  
+    editor
+      .edit((editBuilder) => {
+        if (!selection.isEmpty) {
+          // If there is a selection, replace only the selected text
+          editBuilder.replace(selection, refactoredCode);
+        } else {
+          // If there is no selection, replace the entire document's text
+          const entireRange = new vscode.Range(
+            document.positionAt(0),
+            document.positionAt(document.getText().length)
+          );
+          editBuilder.replace(entireRange, refactoredCode);
+        }
+      })
+      .then((success) => {
+        if (!success) {
+          vscode.window.showErrorMessage("Failed to update the file.");
+        }
+      });
+  }
+
+async function performRefactoring() {
+  const code = getActiveFileContent();
+  if (!code) {
+    vscode.window.showErrorMessage("No active file.");
+    return;
+  }
+
+  try {
+    const refactored = await refactorWithIoenAI(code);
+    updateFileWithRefactoredCode(refactored);
+  } catch (error) {
+    vscode.window.showErrorMessage(`Refactoring failed: ${error.message}`);
+  }
+}
+
+function activate(context) {
+  let disposable = vscode.commands.registerCommand(
+    "refactorme.refactor",
+    function () {
+      performRefactoring();
+    }
+  );
+
+  context.subscriptions.push(disposable);
+}
+
+function deactivate() {}
+
+module.exports = {
+  activate,
+  deactivate,
+};
